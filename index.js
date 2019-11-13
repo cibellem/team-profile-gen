b1183036ecc5e3789cb400e88cc8179eae5ee9d6
Cibelle Montor <montorcibelle@gmail.com>
2019-11-12 19:26:10 -0700

    added functionality and style

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inquirer = require("inquirer");
 
 // Local Modules
 const render = require("./lib/htmlrenderer");
+const questions = require("./lib/questions")
 
 // Constructors
 const Manager = require("./lib/constructors/Manager");
@@ -10,130 +11,53 @@ const Engineer = require("./lib/constructors/Engineer");
 const Intern = require("./lib/constructors/Intern");
 
 // Global Variables
-const teamMembers = [
-  // new Manager("jamon", 150, "brent@gmail.com", 150),
-  // // new Engineer("Tucker", 2, "tbeauchamp@2u.com", "tuckerbeauchamp"),
-  // // new Intern("Becky", 3, "becky@becky.com", "UofA"),
-  // // new Intern("Shelly", 4, "shelly@shelly.com", "UofA")
-];
-
-const jobTitle = [
-
-  {
-    type: "list",
-    name: "title",
-    message: "What is your Job title?",
-    choices: [
-
-      "Manager",
-      "Engineer",
-      "Intern",
-
-    ]
-  },
-
-]
-const managerQuestions = [
-  {
-    type: "input",
-    name: "name",
-    message: "What is your Name?",
-  },
-
-  {
-    type: "number",
-    name: "id",
-    message: "What is your ID?",
-  },
-
-  {
-    type: "input",
-    name: "email",
-    message: "What is your e-mail?",
-  },
-  {
-    type: "number",
-    name: "office",
-    message: "What is your office number?",
-  },
-]
-
-const engineerQuestions = [
-  {
-    type: "input",
-    name: "name",
-    message: "What is your Name?",
-  },
-
-  {
-
-    type: "number",
-    name: "id",
-    message: "What is your ID?",
-  },
-
-  {
-    type: "input",
-    name: "email",
-    message: "What is your e-mail?",
-  },
-
-  {
-    type: "input",
-    name: "github",
-    message: "What is your gitHub Account?",
-  },
-
-
-
-
-]
-
-const internQuestions = [
-  {
-    type: "input",
-    name: "name",
-    message: "What is your Name?",
-  },
-
-  {
-
-    type: "number",
-    name: "id",
-    message: "What is your ID?",
-  },
-
-  {
-    type: "input",
-    name: "email",
-    message: "What is your e-mail?",
-  },
-  {
-    type: "input",
-    name: "school",
-    message: "What is your School?",
-  },
-]
-
-inquirer.prompt(jobTitle).then(function (data) {
-
-  if (data.title === "Manager") {
-    inquirer.prompt(managerQuestions).then(function (res) {
-      teamMembers.push(new Manager(res.name, res.id, res.email, res.office));
-    });
-
-  }
-  if (data.title === "Engineer") {
-    inquirer.prompt(engineerQuestions);
-  }
-  if (data.title === "Intern") {
-    inquirer.prompt(internQuestions);
-  }
-})
+const teamMembers = [];
 
 async function init() {
-  render(teamMembers);
+
+  //starts by asking the manager his info and store into the array teamMembers
+  const response = await inquirer.prompt(questions.managerQuestions)
+  const manager = new Manager(response.name, response.id, response.email, response.office)
+  teamMembers.push(manager);
+  console.log(teamMembers)
+
+  //created a variable to state the condition it's true so I can stop at the end 
+  let newPerson = true;
+
+
+  //the loop will keep going until user states no longer wants to add a new person
+  while (newPerson) {
+    const member = await inquirer.prompt(questions.addPeople)
+    console.log(member)
+
+    //if engineer person being added ask the eng questions and them push to the team member's array
+    if (member.addmore === "Engineer") {
+
+      const res = await inquirer.prompt(questions.engineerQuestions)
+      const engineer = new Engineer(res.name, res.id, res.email, res.github);
+      teamMembers.push(engineer);
+    }
+
+    //if intern person being added ask the intern questions and them push to the team member's array
+    else if (member.addmore === "Intern") {
+
+      const res = await inquirer.prompt(questions.internQuestions);
+      const intern = new Intern(res.name, res.id, res.email, res.school);
+      teamMembers.push(intern);
+    }
+
+    //when user not longer wants to add people, stop the while loop and call the render function so HTML it's created.
+    else {
+      newPerson = !newPerson
+      render(teamMembers)
+
+    }
+
+  }
+
+
 }
+
 
 
 init();
